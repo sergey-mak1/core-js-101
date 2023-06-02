598b335d9534b9f781f1cc30ef3b07a8288da673
sergey-mak1 <sega013021990@gmail.com>
2023-06-02 21:30:05 +0300

    feat:added solution string-tasks

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -297,8 +297,18 @@ function isString(value) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(/* value */) {
-  throw new Error('Not implemented');
+function getCardId(value) {
+  const arrCards = ['A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣',
+    'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦',
+    'A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥',
+    'A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠'];
+  let indexCard;
+  arrCards.forEach((card) => {
+    if (card === value) {
+      indexCard = arrCards.indexOf(card);
+    }
+  });
+  return indexCard;
 }
 
 
